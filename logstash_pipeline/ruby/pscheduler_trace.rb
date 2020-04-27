279635e99f1cd935677f8e8e8508470c71e98162
Andy Lake <andy@es.net>
2020-04-27 08:42:33 -0400

    Adding AS organization to trace results

--- logstash_pipeline/ruby/pscheduler_trace.rb
+++ logstash_pipeline/ruby/pscheduler_trace.rb
@@ -4,6 +4,7 @@ def filter(event)
         #only look at first path for now, multipath is not this use case 
         hop_ips = []
         hop_asns = []
+        hop_asos = []
         path_mtu  = nil
         success_count = 0
         error_count = 0
@@ -18,8 +19,14 @@ def filter(event)
             end
             
             #ASN
-            if hop["as"] and hop["as"]["number"] then
-                hop_asns.push(hop["as"]["number"])
+            if hop["as"] then
+                if hop["as"]["number"] then
+                    hop_asns.push(hop["as"]["number"])
+                end
+                
+                if hop["as"]["owner"] then
+                    hop_asos.push(hop["as"]["owner"])
+                end
             end
             
             #Path MTU
@@ -37,7 +44,8 @@ def filter(event)
         event.set("[@metadata][result][hop][success_count]", success_count)
         event.set("[@metadata][result][hop][error_count]", error_count)
         event.set("[@metadata][result][hop][ip]", hop_ips)
-        event.set("[@metadata][result][hop][asn]", hop_asns)
+        event.set("[@metadata][result][hop][as_number]", hop_asns)
+        event.set("[@metadata][result][hop][as_organization]", hop_asos)
         if path_mtu then
             event.set("[@metadata][result][mtu]", path_mtu)
         end
