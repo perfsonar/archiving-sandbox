8a22bece158c100998e91282c3ad1647f84d3fc2
Andy Lake <andy@es.net>
2020-05-04 22:44:58 -0400

    Adding support for unsummarized histogram-owdely types

--- elmond/elmond/data.py
+++ elmond/elmond/data.py
@@ -4,11 +4,45 @@ from filters import build_time_filter
 
 DEFAULT_RESULT_LIMIT=1000
 MAX_RESULT_LIMIT=10000
+#TODO: DELETE THIS
+SUPPORTED_SUMMARIES = {
+    "base": True,
+    "statistics": True
+}
 DATA_FIELD_MAP = {
-    "throughput": "result.throughput_bits"
+    "throughput/base": "result.throughput_bits",
+    "histogram-owdelay/base": "result.latency.histogram",
+    "histogram-owdelay/statistics": [
+        "result.latency.max",
+        "result.latency.mean",
+        "result.latency.median",
+        "result.latency.min",
+        "result.latency.mode",
+        "result.latency.p_25",
+        "result.latency.p_75",
+        "result.latency.p_95",
+        "result.latency.stddev",
+        "result.latency.variance"
+    ]
 }
 log = logging.getLogger('elmond')
 
+def _build_esmond_histogram(elastic_histo):
+    values = elastic_histo.get("values", [])
+    counts = elastic_histo.get("counts", [])
+    #make sure same length
+    num_buckets = len(values)
+    if  num_buckets != len(counts):
+        return None
+    #build new type
+    esmond_histo = {}
+    for i in range(num_buckets):
+        try:
+            esmond_histo[str(values[i])] = int(counts[i])
+        except ValueError as e:
+            return None
+    
+    return esmond_histo
 
 class EsmondData:
 
@@ -23,8 +57,8 @@ class EsmondData:
             summary_window = int(summary_window)
         except ValueError as e:
             raise BadRequest("Summary window must be an int")
-        if summary_type != "base" or summary_window != 0:
-            raise NotImplemented("Currently only base summaries with window 0 are supported")
+        if summary_type not in SUPPORTED_SUMMARIES or summary_window != 0:
+            raise NotImplemented("Currently only base and statistics summaries with window 0 are supported")
         
         #handle limit and offset
         result_size = DEFAULT_RESULT_LIMIT
@@ -73,8 +107,12 @@ class EsmondData:
             dsl["query"]["bool"]["filter"].append(time_filter)
         
         #limit fields returned
-        if event_type in DATA_FIELD_MAP:
-            dsl["_source"].append(DATA_FIELD_MAP[event_type])
+        dfm_key = "{0}/{1}".format(event_type, summary_type)
+        if dfm_key in DATA_FIELD_MAP:
+            if isinstance(DATA_FIELD_MAP[dfm_key], list):
+                dsl["_source"].extend(DATA_FIELD_MAP[dfm_key])
+            else:
+                dsl["_source"].append(DATA_FIELD_MAP[dfm_key])
         else:
             dsl["_source"].append("result.*")
         
@@ -91,10 +129,34 @@ class EsmondData:
             if not ts or not result:
                 continue
             datum = { "ts": datestr_to_timestamp(ts) }
-            #get value - event type specific
+            #get value - event type specific. 
+            # Note: right now it either spits out an empty string or just gives raw results for unsupported event types
             if event_type == "throughput":
                 if "throughput_bits" in result:
                     datum["val"] = result["throughput_bits"]
+                else:
+                    continue
+            elif event_type == "histogram-owdelay" and summary_type == "statistics":
+                if "latency" in result:
+                    datum["val"] = {
+                        "maximum": result["latency"].get("max", None),
+                        "mean": result["latency"].get("mean", None),
+                        "median": result["latency"].get("median", None),
+                        "minimum": result["latency"].get("min", None),
+                        "mode": result["latency"].get("mode", None),
+                        "percentile-25": result["latency"].get("p_25", None),
+                        "percentile-75": result["latency"].get("p_75", None),
+                        "percentile-95": result["latency"].get("p_95", None),
+                        "standard-deviation": result["latency"].get("stddev", None),
+                        "variance": result["latency"].get("variance", None)
+                    }
+                else:
+                    continue
+            elif event_type == "histogram-owdelay":
+                if "latency" in result and "histogram" in result["latency"]:
+                    datum["val"] = _build_esmond_histogram(result["latency"]["histogram"])
+                    if datum["val"] is None:
+                        continue
                 else:
                     continue
             else:
